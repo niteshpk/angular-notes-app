a316a15c95eea3ea4b069647ab51b0aefd461dd4
Nitesh Kesarkar <jyoti@infrrd.ai>
2020-11-04 20:56:33 +0530

    added basic template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
   {
-    path: 'test',
-    component: TestComponent
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'posts',
+    component: PostsComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostsComponent } from './posts/posts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    PostsComponent,
+    DashboardComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
